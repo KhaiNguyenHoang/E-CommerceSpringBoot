85572ba5cfb24ac52f545b1fd05f9625d342bd81
KhaiNguyenHoang <khaiproject1234@gmail.com>
2025-06-25 17:11:41 +0700

    Update EComerce

--- src/main/java/com/example/ecommercespringboot/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/ecommercespringboot/configuration/SecurityConfiguration.java
@@ -3,19 +3,30 @@ package com.example.ecommercespringboot.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
     private final JwtDecoder jwtDecoder;
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
 
-    public SecurityConfiguration(JwtDecoder jwtDecoder) {
+    public SecurityConfiguration(JwtDecoder jwtDecoder, JwtAuthenticationConverter jwtAuthenticationConverter) {
         this.jwtDecoder = jwtDecoder;
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
     }
 
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf -> csrf.disable())
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder)))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.decoder(jwtDecoder).jwtAuthenticationConverter(jwtAuthenticationConverter)))
                 .build();
+    }
+
+    public JwtDecoder jwtDecoder() {
+        return jwtDecoder;
+    }
+
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return jwtAuthenticationConverter;
     }
 }
